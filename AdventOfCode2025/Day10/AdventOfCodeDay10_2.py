b7a8fd45775ff162458dfb26f3b007d6498ad8c4
AlexMovsess <73705463+AlexMovsess@users.noreply.github.com>
2026-05-01 19:31:18 +0200

    Advent of code day 10_2 solution start

--- AdventOfCode2025/Day10/AdventOfCodeDay10_2.py
+++ AdventOfCode2025/Day10/AdventOfCodeDay10_2.py
@@ -1,77 +1,72 @@
 import os
-from sympy import symbols, Eq, solve
+import re
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-with open(os.path.join(__location__, "AdventOfCodeDay10.txt")) as f:
-    content = f.read()
+# (3,4,5,7) (2,4,5,6,7) (1,4,7) (1,3,4,7) (1,2,3,4,5,7) (7) (1,2,3,6) (0,1,3,6,7) {4,59,39,250,242,220,26,250}
 
 # This is an equation resolution / matrix simplification. number of variables is length of lights. We need to find all solutions and then pick the smallest ?
 
-# (3,4,5,7) (2,4,5,6,7) (1,4,7) (1,3,4,7) (1,2,3,4,5,7) (7) (1,2,3,6) (0,1,3,6,7) {4,59,39,250,242,220,26,250}
+total = 0
 
-a, b, c, d, e, f, g, h = symbols("a b c d e f g h")
+with open(os.path.join(__location__, "AdventOfCodeDay10.txt")) as f:
+    content = f.read()
+    lines = content.split("\n")
 
-equation_1 = Eq((h), 4)
-equation_2 = Eq((c + d + e + g + h), 59)
-equation_3 = Eq((b + e + g), 39)
-equation_4 = Eq((a + d + e + g + h), 250)
-equation_5 = Eq((a + b + c + d + e), 242)
-equation_6 = Eq((a + b + e), 220)
-equation_7 = Eq((b + g + h), 26)
-equation_8 = Eq((a + b + c + d + e + f + h), 250)
+    for line in lines:
 
-solution = solve(
-    (
-        equation_1,
-        equation_2,
-        equation_3,
-        equation_4,
-        equation_5,
-        equation_6,
-        equation_7,
-        equation_8,
-    ),
-    (a, b, c, d, e, f, g, h),
-)
-print("Solution:", solution)
+        joltage_goal = list(
+            map(lambda x: int(x), re.findall(r"\{(.*?)\}", line)[0].split(","))
+        )
 
-print(sum(solution.values()))
+        buttons = re.findall(r"\((.*?)\)", line)
+        buttons = [list(map(int, p.split(","))) for p in buttons]
+        buttons.sort(key=lambda x: len(x), reverse=True)
 
-# (1,2,3,4,5,6,7,8,9) (1,5) (0,1,3,4,5,6,7,8,9) (4,8,9) (0,6,7,9) (1,6,8) (0,1,2,5,6,7,8) (0,1,2,3,5,7,8,9) (1,2,4,5) {27,79,30,21,45,59,54,36,50,43}
+        finished = False
+        pushes = 0
 
-a, b, c, d, e, f, g, h, i, j = symbols("a b c d e f g h i j")
+        joltage_goal_copy = joltage_goal.copy()
 
-variable_list = [a, b, c, d, e, f, g, h, i]
+        while not finished:
 
-variable = variable_list[2] + e + g + h
-print(variable)
-equation_1 = Eq(variable, 27)
-equation_2 = Eq((a + b + c + f + g + h + i), 79)
-equation_3 = Eq((a + g + h + i), 30)
-equation_4 = Eq((a + c + h), 21)
-equation_5 = Eq((a + c + d + i), 45)
-equation_6 = Eq((a + b + c + g + h + i), 59)
-equation_7 = Eq((a + c + e + f + g), 54)
-equation_8 = Eq((a + c + e + g + h), 36)
-equation_9 = Eq((a + c + d + f + g + h), 50)
-equation_10 = Eq((a + c + d + e + h), 43)
+            minimum_joltage = 10000000
+            if sum(joltage_goal_copy) == 0:
+                break
 
-solution = solve(
-    (
-        equation_1,
-        equation_2,
-        equation_3,
-        equation_4,
-        equation_5,
-        equation_6,
-        equation_7,
-        equation_8,
-        equation_9,
-        equation_10,
-    ),
-    (a, b, c, d, e, f, g, h, i, j),
-)
-print("Solution:", solution)
+            print(joltage_goal_copy)
 
-print(sum(solution.values()))
+            for joltage in joltage_goal_copy:
+                print(joltage)
+                if joltage < minimum_joltage and joltage > 0:
+                    minimum_joltage = joltage
+
+            index_minimum_joltage = joltage_goal_copy.index(minimum_joltage)
+
+            print(buttons)
+            print(index_minimum_joltage, minimum_joltage)
+
+            for button in buttons:
+
+                if index_minimum_joltage in button:
+                    print(button)
+                    print(joltage_goal_copy)
+                    right_button = True
+
+                    for index in button:
+                        if joltage_goal_copy[index] - minimum_joltage < 0:
+                            right_button = False
+
+                    if right_button:
+                        for index in button:
+                            joltage_goal_copy[index] -= minimum_joltage
+                        pushes += minimum_joltage
+                    else:
+                        continue
+
+                    input()
+                    break
+
+        total += pushes
+        print(pushes)
+print(total)
